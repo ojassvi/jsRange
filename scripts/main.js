336eeac08decb8baa0124c373a03bef8f3697424
Ojassvi Chawla <ojassvi@hotmail.com>
2015-04-06 13:33:08 +0530

    Minimised the project to bare minimum. 	- removed bower, and yo. 	- Removed grunt's several build tasks 	- Moved style files and less files to top directory level.

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,26 @@
 (function () {
+    'use strict';
     var doc = document,
-        leftHandle = doc.querySelector(".slider-handle.left-handle"),
-        rightHandle = doc.querySelector(".slider-handle.right-handle");
-
-    console.log(leftHandle.nodeType);
-    console.log(rightHandle.nodeType);
-
-    var flag = 0,
+        leftHandle = doc.querySelector('.slider-handle.left-handle'),
+        rightHandle = doc.querySelector('.slider-handle.right-handle'),
+        flag = 0,
         startFlag = 0;
-    // var element = xxxx;
-    leftHandle.addEventListener("mousedown", function(){
+
+    leftHandle.addEventListener('mousedown', function() {
         flag = 0;
         startFlag = 1;
     }, false);
-    leftHandle.addEventListener("mousemove", function(e){
+
+    leftHandle.addEventListener('mousemove', function(e) {
         flag = 1;
         if(startFlag === 1) {
-            console.log(e.clientX, e.clientX, e.movementX, e.movementY);
+            // console.log(e.clientX, e.clientX, e.movementX, e.movementY);
             var y = e.movementY;
             if(y > 0) {
                 var left = leftHandle.style.left;
 
                 if(left) {
-                    left = left.replace("px", "");
+                    left = left.replace('px', '');
                     left = parseInt(left, 10);
                 }
                 else {
@@ -30,18 +28,20 @@
                 }
 
                 left += y;
-                left += "px";
+                left += 'px';
                 leftHandle.style.left = left;
             }
         }
     }, false);
-    leftHandle.addEventListener("mouseup", function(){
+
+    leftHandle.addEventListener('mouseup', function() {
         startFlag = 0;
+        console.log('fired');
         if(flag === 0) {
-            console.log("click");
+            console.log('click');
         }
         else if(flag === 1) {
-            console.log("drag");
+            console.log('drag');
         }
     }, false);
 
