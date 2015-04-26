83018aaa8651c0263b670d8d4ba506b5e915b5b3
Ojassvi Chawla <ojassvi@hotmail.com>
2015-04-26 15:28:15 +0530

    Left and right both sliders are moving.

--- scripts/jsRange.js
+++ scripts/jsRange.js
@@ -1,22 +1,66 @@
 (function () {
     'use strict';
 
+    function Handle(element) {
+        this.handle = element;
+        this.start = element.style.left.replace("px", "") || 0;
+        this.move = this.start;
+        var self = this;
+
+        this.mousedown = function (e) {
+            e.preventDefault();
+            self.start = e.pageX - self.move;
+            this.addEventListener('mousemove', self.mousemove);
+            document.addEventListener('mouseup', self.mouseup);
+        };
+
+        this.mousemove = function (e) {
+            e.preventDefault();
+            self.move = e.pageX - self.start;
+            self.css({
+                left:  self.move + 'px'
+            });
+        };
+
+        this.mouseup = function (e) {
+            e.preventDefault();
+            console.log("yo", this);
+            self.handle.removeEventListener('mousemove', self.mousemove);
+            document.removeEventListener('mouseup', self.mouseup);
+        };
+
+        this.css = function(params) {
+            for(var p in params) {
+                this.handle.style[p] = params[p];
+            }
+        };
+
+        this.handle.addEventListener('mousedown', this.mousedown);
+    }
+
     /*jshint validthis:true */
     function jsRange(element, options) {
-        this.leftHandle = element.querySelector('.slider-handle.left-handle');
-        this.rightHandle = element.querySelector('.slider-handle.right-handle');
+        // this.leftHandle = element.querySelector('.slider-handle.left-handle');
+        // this.rightHandle = element.querySelector('.slider-handle.right-handle');
 
-        options = options || {};
+        var jsRangeWidth = window.getComputedStyle(element).width;
+        element.querySelector('.slider-handle.right-handle').style.left = jsRangeWidth;
 
-        var down = false,
-            width = options.width || 10,
-            prevEvt,
-            left = this.leftHandle.style.left,
-            leftWidth = this.leftHandle.style.width,
-            rangeWidth,
-            rangeLeft;
+        var leftHandle = new Handle(element.querySelector('.slider-handle.left-handle')),
+            rightHandle = new Handle(element.querySelector('.slider-handle.right-handle'));
 
-        var startX = 0, startY = 0, x = 0, y = 0;
+
+        // options = options || {};
+
+        // var down = false,
+        //     width = options.width || 10,
+        //     prevEvt,
+        //     left = this.leftHandle.style.left,
+        //     leftWidth = this.leftHandle.style.width,
+        //     rangeWidth,
+        //     rangeLeft;
+
+        // var startX = 0, startY = 0, x = 0, y = 0;
 
         // this.leftHandle.addEventListener('mousedown', function(e) {
         //     // rangeWidth = this.offsetWidth;
@@ -35,82 +79,46 @@
         //     // return false;
         // }, false);
 
-        var leftHandle = this.leftHandle,
-            rightHandle = this.rightHandle;
+        // var leftHandle = this.leftHandle,
+        //     rightHandle = this.rightHandle;
 
-        function mousemove(e) {
-            // console.log(this);
-            // y = e.pageY - startY;
-            x = e.pageX - startX;
-            css({
-                // top: y + 'px',
-                left:  x + 'px'
-            });
-        }
+        // function mousemove(e) {
+        //     x = e.pageX - startX;
+        //     css({
+        //         left:  x + 'px'
+        //     });
+        // }
 
-        function mousedown(e) {
-            startX = e.pageX - x;
-            leftHandle.addEventListener('mousemove', mousemove);
-            leftHandle.addEventListener('mouseup', mouseup);
-        }
+        // function mousedown(e) {
+        //     startX = e.pageX - x;
+        //     leftHandle.addEventListener('mousemove', mousemove);
+        //     leftHandle.addEventListener('mouseup', mouseup);
+        // }
 
 
-        leftHandle.addEventListener('mousedown', mousedown);
+        // leftHandle.addEventListener('mousedown', mousedown);
 
 
-        function mouseup() {
-            console.log("in");
-            leftHandle.removeEventListener('mousemove', mousemove);
-            leftHandle.removeEventListener('mouseup', mouseup);
-        }
+        // function mouseup() {
+        //     leftHandle.removeEventListener('mousemove', mousemove);
+        //     leftHandle.removeEventListener('mouseup', mouseup);
+        // }
 
-        function css(params) {
-            for(var p in params) {
-                leftHandle.style[p] = params[p];
-            }
-        }
+        // function css(params) {
+        //     for(var p in params) {
+        //         leftHandle.style[p] = params[p];
+        //     }
+        // }
 
-        // this.leftHandle.addEventListener('mousemove', function(e) {
-        //     // if(down) {
-        //     //     console.log(this.offsetWidth, this.offsetLeft);
-        //     //     // console.log(e.clientX, e.clientX, e.movementX, e.movementY);
-        //     //     var prevY = prevEvt ? prevEvt.screenY : 0;
-        //     //     // console.log(e.screenY, prevY);
-        //     //     var y = e.screenY - prevY;
-
-        //     //     // console.log(y);
-        //     //     if(y > 0) {
-        //     //         if(left) {
-        //     //             left = left.replace('px', '');
-        //     //             left = parseInt(left, 10);
-        //     //         }
-        //     //         else {
-        //     //             left = 0;
-        //     //         }
-
-        //     //         left += 1;
-        //     //         left += 'px';
-        //     //         this.style.left = left;
-        //     //     }
-        //     // }
-
-        //     updateDragger(e);
-        // }, false);
-
-        // this.leftHandle.addEventListener('mouseup', function(e) {
-        //     down = false;
-        // }, false);
-
-        function updateDragger(e) {
-            console.log(e);
-            if (down && e.pageX >= rangeLeft && e.pageX <= (rangeLeft + rangeWidth)) {
-                console.log("yo");
-                this.leftHandle.style.left = e.pageX - rangeLeft - leftWidth + 'px';
-                if (typeof options.onDrag === "function") {
-                    options.onDrag(Math.round(((e.pageX - rangeLeft) / rangeWidth) * 100));
-                }
-            }
-        }
+        // function updateDragger(e) {
+        //     if (down && e.pageX >= rangeLeft && e.pageX <= (rangeLeft + rangeWidth)) {
+        //         console.log("yo");
+        //         this.leftHandle.style.left = e.pageX - rangeLeft - leftWidth + 'px';
+        //         if (typeof options.onDrag === "function") {
+        //             options.onDrag(Math.round(((e.pageX - rangeLeft) / rangeWidth) * 100));
+        //         }
+        //     }
+        // }
     }
 
     window.jsRange = jsRange;
